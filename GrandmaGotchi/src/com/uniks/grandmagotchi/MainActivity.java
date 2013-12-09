a6912c542031e306c8609f358c112b26564ed404
maximlueck <maximlueck@googlemail.com>
2013-12-09 14:12:58 +0000

    Add StartWindow

--- GrandmaGotchi/src/com/uniks/grandmagotchi/MainActivity.java
+++ GrandmaGotchi/src/com/uniks/grandmagotchi/MainActivity.java
@@ -3,8 +3,12 @@ package com.uniks.grandmagotchi;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
+	
+	public GrannyAttributes grannyAttributes = new GrannyAttributes();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +22,16 @@ public class MainActivity extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
+    }
+    
+    public void btnClickStartGame(View view) {
+    	
+    	setContentView(R.layout.living_room);
+    	EditText fieldGrannyName = (EditText)findViewById(R.id.tfGrannysName);
+    	grannyAttributes.setName(fieldGrannyName.getText().toString());
+    	
+    	
+    	
     }
     
 }
