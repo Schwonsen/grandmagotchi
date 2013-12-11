2f902818b08ea3f8c365be9ed76b63685207409b
mirkosartorius89@gmail.com <mirkosartorius89@gmail.com@2e4f3cd2-677a-56f3-12c8-3170ece29b1f>
2013-12-11 16:05:32 +0000

    login v1.2 and adding the rooms

---
+++ trunk/GrandmaGotchi/src/com/uniks/grandmagotchi/FragmentLivingRoom.java
@@ -0,0 +1,30 @@
+package com.uniks.grandmagotchi;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link android.support.v4.app.Fragment} subclass.
+ * 
+ */
+public class FragmentLivingRoom extends Fragment
+{
+
+	public FragmentLivingRoom()
+	{
+		// Required empty public constructor
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState)
+	{
+		// Inflate the layout for this fragment
+		return inflater.inflate(R.layout.fragment_living_room,
+				container, false);
+	}
+
+}
