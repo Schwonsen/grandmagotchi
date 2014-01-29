50f035f9b3c3d865d2772e6d2db18b46372f1bb0
mirkosartorius89 <mirkosartorius89@gmail.com>
2014-01-29 13:27:47 +0000

    new fragments

---
+++ GrandmaGotchi/src/com/uniks/grandmagotchi/rooms/FragmentSupermarket.java
@@ -0,0 +1,32 @@
+package com.uniks.grandmagotchi.rooms;
+
+import com.uniks.grandmagotchi.R;
+import com.uniks.grandmagotchi.R.layout;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link android.support.v4.app.Fragment} subclass.
+ * 
+ */
+public class FragmentSupermarket extends Fragment
+{
+
+	public FragmentSupermarket()
+	{
+		// Required empty public constructor
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState)
+	{
+		// Inflate the layout for this fragment
+		return inflater.inflate(R.layout.fragment_supermarket, container, false);
+	}
+
+}
