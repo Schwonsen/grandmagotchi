68d12e14f3f2064b212c4693021e0d40635db3e1
maximlueck <maximlueck@googlemail.com>
2014-02-04 20:30:36 +0000

    lists

---
+++ GrandmaGotchi/src/com/uniks/grandmagotchi/data/ClotheAdapter.java
@@ -0,0 +1,119 @@
+package com.uniks.grandmagotchi.data;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import com.uniks.grandmagotchi.R;
+import com.uniks.grandmagotchi.WarderobeActivity;
+import com.uniks.grandmagotchi.util.Root;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class ClotheAdapter extends BaseAdapter {
+
+	List<HashMap<String, String>> data;
+	ViewHolder holder;
+	LayoutInflater inflater;
+
+	public ClotheAdapter() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public ClotheAdapter(Activity a, List<HashMap<String, String>> d) {
+		this.data = d;
+		inflater = (LayoutInflater) a
+				.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+	}
+
+	@Override
+	public int getCount() {
+		// TODO Auto-generated method stub
+		return data.size();
+	}
+
+	@Override
+	public Object getItem(int position) {
+		// TODO Auto-generated method stub
+		return position;
+	}
+
+	@Override
+	public long getItemId(int position) {
+		// TODO Auto-generated method stub
+		return position;
+	}
+
+	static class ViewHolder {
+		TextView meal;
+		TextView information;
+		ImageView ic_image;
+		TextView tvFoodCount;
+	}
+
+	public View getView(int position, View convertView, ViewGroup parent) {
+		View vi = convertView;
+
+		if (convertView == null) {
+			vi = inflater.inflate(R.layout.meal_list_row, null);
+			holder = new ViewHolder();
+
+			holder.meal = (TextView) vi.findViewById(R.id.tvMeal); // meal name
+			holder.information = (TextView) vi.findViewById(R.id.tvInformation); // lunch time
+			holder.ic_image = (ImageView) vi.findViewById(R.id.list_image); // meal image
+			holder.tvFoodCount = (TextView) vi.findViewById(R.id.tvFoodCount);
+
+			vi.setTag(holder);
+
+		} else {
+			
+			holder = (ViewHolder) vi.getTag();
+		}
+
+		// // Setting all values in listview
+		holder.meal.setText(data.get(position).get(WarderobeActivity.KEY_CLOTHE));
+		holder.information.setText(data.get(position).get(WarderobeActivity.KEY_TIME));
+
+		// Setting an image
+		String uri = "drawable/" + data.get(position).get(WarderobeActivity.KEY_ICON);
+		int imageResource = vi
+				.getContext()
+				.getApplicationContext()
+				.getResources()
+				.getIdentifier(
+						uri,
+						null,
+						vi.getContext().getApplicationContext()
+								.getPackageName());
+		Drawable image = vi.getContext().getResources()
+				.getDrawable(imageResource);
+		holder.ic_image.setImageDrawable(image);
+		
+		String counter = data.get(position).get(WarderobeActivity.KEY_COUNT);
+		Iterator it = data.get(position).entrySet().iterator();
+		while (it.hasNext()) 
+		{
+		        Map.Entry pairs = (Map.Entry)it.next();
+//		        System.out.println(pairs.getKey() + " = " + pairs.getValue());
+		        for(ClotheAttributes clotheItem : Root.getClotheList())
+		        {
+		        	if(pairs.getValue().equals(clotheItem.getName()))
+		        	{
+		        		holder.tvFoodCount.setText(String.valueOf(clotheItem.getCount()));
+		        	}
+		        }
+		 }
+
+		return vi;
+
+	}
+}
---
+++ GrandmaGotchi/src/com/uniks/grandmagotchi/data/ClotheAttributes.java
@@ -0,0 +1,26 @@
+package com.uniks.grandmagotchi.data;
+
+public class ClotheAttributes 
+{
+	
+	private String name;
+	private int count;
+	
+	public String getName()
+	{
+		return name;
+	}
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+	public int getCount()
+	{
+		return count;
+	}
+	public void setCount(int count)
+	{
+		this.count = count;
+	}
+
+}
